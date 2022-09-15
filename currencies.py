26c21f893b57e0d933cea56a427d53c1ebecf478
Ismael Laet <ismaelabashi@hotmail.com>
2022-09-15 20:44:55 -0300

    feat: Get currencies through user input

--- currencies.py
+++ currencies.py
@@ -6,3 +6,17 @@ class Currencies:
     def show_currencies(self):
         for key, value in self.available_currencies.items():
             print(f'{value:24} -> {key:>4}')
+
+    def get_user_input_currencies(self):
+        while True:
+            from_currency = input('\nFrom currency -> ').upper()
+            to_currency = input('To currency -> ').upper()
+            currencies_exist = self.validation(from_currency, to_currency)
+
+            if currencies_exist:
+                return (from_currency, to_currency)
+            print('\nInvalid currencies')
+
+
+    def validation(self, currency1, currency2):
+        return all(currency in self.available_currencies for currency in (currency1, currency2))
