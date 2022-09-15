7d634ccca2b2d4a173f9145f772a5b30372fa298
Ismael Laet <ismaelabashi@hotmail.com>
2022-09-15 20:28:41 -0300

    feat: Grub with the option show currencies

--- main.py
+++ main.py
@@ -1,10 +1,36 @@
 import requests
+from currencies import Currencies
 
-
-host = 'https://www.frankfurter.app/'
 
 def main():
-    pass
+    host = 'https://www.frankfurter.app/'
+
+    try:
+        available_currencies = requests.get(f'{host}/currencies').json()
+    except:
+        print('Server Error!')
+        return
+
+    currencies = Currencies(available_currencies=available_currencies)
+    while True:
+        action = set_action_grub()
+        if action == 's':
+            currencies.show_currencies()
+            pause = input("\nPress the any key to continue...")
+        break
+
+
+def set_action_grub():
+    while True:
+        action = input('\nPress:\n'
+                    '   s - Show currencies available\n'
+                    '   c - Conversion\n'
+                    '   q - Quit\n'
+                    '>> ').lower()
+
+        if action in ('s', 'c', 'q'):
+            return action
+        print('\nInvalid answer!')
 
 if __name__ == '__main__':
     main()
